791269d797b22c27853186c30250069242cc1e86
hai <hai.alphacreative@gmail.com>
2024-08-21 11:31:46 +0700

    update code

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  console.log("ready!");
+  scrollHeader();
+});
+
+function scrollHeader() {
+  let height = $(".header__active").height() * -1;
+  console.log(height);
+  function initializeScrollTrigger() {
+    navTop = gsap
+      .from(".header__active", {
+        y: height,
+        paused: true,
+        duration: 0.5,
+        ease: "power1.out",
+        trigger: "header",
+      })
+      .progress(1);
+
+    ScrollTrigger.create({
+      start: "top top",
+      end: 99999,
+      onUpdate: (self) => {
+        // Shrink navTop
+        self.direction === -1 ? navTop.play() : navTop.reverse();
+        // self.refresh();
+        if (self.direction === -1) {
+          $(".header__default").removeClass("scrolled-down");
+        } else {
+          $(".header__default").addClass("scrolled-down");
+        }
+      },
+    });
+  }
+
+  initializeScrollTrigger();
+
+  // Re-initialize ScrollTrigger when page is refreshed
+  $(window).on("load", initializeScrollTrigger);
+}
