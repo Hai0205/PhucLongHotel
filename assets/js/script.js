56651b0ad64c0c00ee1b393f6818b8cc1dc92bf7
hai <hai.alphacreative@gmail.com>
2024-08-21 17:53:10 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
   console.log("ready!");
   scrollHeader();
+  swiperHotels();
 });
 
 function scrollHeader() {
   let height = $(".header__active").height() * -1;
-  console.log(height);
   function initializeScrollTrigger() {
     navTop = gsap
       .from(".header__active", {
@@ -37,4 +37,23 @@ function scrollHeader() {
 
   // Re-initialize ScrollTrigger when page is refreshed
   $(window).on("load", initializeScrollTrigger);
+}
+
+function swiperHotels() {
+  if ($(".swiper-tab").length && $(".nav-link.active").length) {
+    const swiperTab = new Swiper(".swiper-tab", {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    });
+
+    function updateSlideCount() {
+      const activeSlideCount = $(
+        ".tab-pane.show .swiper-tab .swiper-slide"
+      ).length;
+      console.log("Số lượng swiper-slide đang hoạt động:", activeSlideCount);
+    }
+
+    updateSlideCount();
+    $('button[data-bs-toggle="tab"]').on("shown.bs.tab", updateSlideCount);
+  }
 }
